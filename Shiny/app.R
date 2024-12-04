302fc1f1dde2cc84e828d19018c7330e6549b70d
Fl-ris <florismenninga@gmail.com>
2024-12-04 09:20:42 +0100

    Improved shiny app wireframe and added normality test for data.

--- Shiny/app.R
+++ Shiny/app.R
@@ -1,16 +1,21 @@
 library(shiny)
+library(shinydashboard)
 library(bslib)
 library(here)
 library(tidyverse)
 
+library(plotly)
+
+bslib::page_sidebar(...)
 
 # Source the UI object from the following file:
-source(file = "/home/floris/Documenten/git_repo/Lifelines_data_dashboard/Shiny/ui.R")
+source(file = "/home/floris/Documents/git_repo/Lifelines_data_dashboard/Shiny/ui.R")
+
 
 #source(here::here('ui.R'))
 
 # Source the Server object from the following file:
-source(file = "/home/floris/Documenten/git_repo/Lifelines_data_dashboard/Shiny/server.R")
+source(file = "/home/floris/Documents/git_repo/Lifelines_data_dashboard/Shiny/server.R")
 
 
 shinyApp(ui = ui, server = server)
--- Shiny/ui.R
+++ Shiny/ui.R
@@ -1,9 +1,30 @@
 ui <- page_sidebar(
-  # App title ----
-  title = "Lifelines:",
-  # Sidebar panel for inputs ----
+
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Correlations", plotOutput("plot")),
+      tabPanel("General statistics", verbatimTextOutput("summary")),
+      tabPanel("Test", tableOutput("table"))
+    )
+  ),
+
+
   sidebar = sidebar(
-    # Input: Slider for the number of bins ----
+    
+    selectInput(
+      inputId="input_1",
+      label="What to compare?",
+      choices=list(`East Coast` = list("Participant", "Province", "CT"),
+           `cat2` = list("aa", "bb", "cc"),
+           `cat3` = list("aa", "aa", "a")),
+      selected = NULL,
+      multiple = FALSE,
+      selectize = TRUE,
+      width = NULL,
+      size = NULL
+    ),
+
+    
     sliderInput(
       inputId = "bins",
       label = "Number of bins:",
@@ -11,14 +32,27 @@ ui <- page_sidebar(
       max = 50,
       value = 30
     ),
+    
     sliderInput(
       inputId = "binss",
       label = "test2:",
       min = 1,
       max = 50,
       value = 30
+    ),
+
+    selectInput(
+      inputId="input_2",
+      label="Plot type to visualize:",
+      choices=list("Automatic", "Scatter plot", "blablabla"),
+      selected = NULL,
+      multiple = FALSE,
+      selectize = TRUE,
+      width = NULL,
+      size = NULL
     )
     
+
     
   ),
   plotOutput(outputId = "distPlot")
